7438359c59f9e8f9115fe2d26b65ab2206b228fc
YasserAhmedMoh <48395651+YasserAhmedMoh@users.noreply.github.com>
2021-10-23 21:58:02 +0200

    Update twi.c

--- Eclipse_code_mcu2/mcu_2/twi.c
+++ Eclipse_code_mcu2/mcu_2/twi.c
@@ -6,7 +6,7 @@
  *
  * Description: Source file for the TWI(I2C) AVR driver
  *
- * Author: Mohamed Tarek
+ * Author: YASSER AHMED
  *
  *******************************************************************************/
  
