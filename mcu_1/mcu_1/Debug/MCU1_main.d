ea3d328f9b7fdff8ab8ba496cdc75ac44a6384e5
YasserAhmedMoh <48395651+YasserAhmedMoh@users.noreply.github.com>
2021-10-28 08:16:01 +0200

    Add files via upload

---
+++ mcu_1/mcu_1/Debug/MCU1_main.d
@@ -0,0 +1,19 @@
+MCU1_main.o MCU1_main.o: ../MCU1_main.c ../timer.h ../std_types.h \
+  ../common_macros.h ../gpio.h ../micro_config.h ../lcd.h ../keypad.h \
+  ../uart.h
+
+../timer.h:
+
+../std_types.h:
+
+../common_macros.h:
+
+../gpio.h:
+
+../micro_config.h:
+
+../lcd.h:
+
+../keypad.h:
+
+../uart.h:
